f5eb0dde063e4c8c275edc006bdc733b08508648
aisgho <tigerkang06@gmail.com>
2026-05-08 10:58:16 +0900

    Update skeleton_cv.py

--- skeleton_cv.py
+++ skeleton_cv.py
@@ -150,7 +150,7 @@ def calculate_angle(line):
 
 def line_point_distance(a, b, x, y):
         
-    distance = float(sqrt((y-a*x-b)**2)/sqrt(a**2+b**2)) # TODO
+    distance = float(np.sqrt((y-a*x-b)**2)/np.sqrt(a**2+b**2)) # TODO
 
     return distance
 
